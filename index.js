c1d0b51c1c414e26954ba8f40fb7392fda6904d3
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-08-21 09:09:42 +0545

    creating custom module in node

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require("http"); // node js inbuilt package
+const myModules = require("./myCustomModule"); // using custom module
 
 http.createServer((req, res)=> {
+    
+    const date = myModules.myDate();
+    const time = myModules.myTime();
+
     res.writeHead(200, {"content-type":"text/html"}); // html tag
-    res.write("<h1>Hello World</h1>"); // body
+    res.write(`<h1>Hello World</h1><p>${date}</p><p>${time}</p>`); // body
     res.end();
+    
 }).listen(5555)
 
 console.log("App is running on port 5555");
